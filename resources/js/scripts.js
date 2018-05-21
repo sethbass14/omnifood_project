e1c570951466e503d130155993324ef3f9d02311
sethbass14 <github email address>
2018-05-21 11:41:05 -0400

    vanilla JS sticky navigation

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,18 @@
-$(document).ready(() => {
-  
+document.addEventListener('DOMContentLoaded', () => {
+  const sectionFeatures = document.getElementsByClassName('section-features')[0]
+  const nav = document.getElementsByTagName('nav')[0]
 
-  
+  document.addEventListener('scroll', () => stickyify(sectionFeatures, nav))
 })
+
+const scrolledPast = element => {
+  let bodyRect = document.body.getBoundingClientRect();
+  let elemRect = element.getBoundingClientRect();
+  let offSet = elemRect.top - bodyRect.top
+  const currentHeight = window.scrollY
+  return offSet <= currentHeight
+}
+
+const stickyify = (scrollEl, navEl) => {
+  navEl.className = scrolledPast(scrollEl) ? 'sticky' : ''
+}
