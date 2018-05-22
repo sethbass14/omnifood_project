d0654a6920709e04133ba017ba17a26e304d7543
sethbass14 <github email address>
2018-05-22 09:23:22 -0400

    fixed some sticky nav formatting

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -5,14 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('scroll', () => stickyify(sectionFeatures, nav))
 })
 
-const scrolledPast = element => {
+const scrolledPast = (scrollEl, navEl) => {
   let bodyRect = document.body.getBoundingClientRect();
-  let elemRect = element.getBoundingClientRect();
-  let offSet = elemRect.top - bodyRect.top - 120
+  let scrollRect = scrollEl.getBoundingClientRect();
+  // 80 is the height of the sticky nav bar. Offsetting here makes the nav appear as its height hits the next section.
+  let offSet = scrollRect.top - bodyRect.top - 80
   const currentHeight = window.scrollY
   return offSet <= currentHeight
 }
 
 const stickyify = (scrollEl, navEl) => {
-  navEl.className = scrolledPast(scrollEl) ? 'sticky' : ''
+  navEl.className = scrolledPast(scrollEl, navEl) ? 'sticky' : ''
 }
